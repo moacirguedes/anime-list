67aad2e8f82aadb721ad8df8e88f678f0506e656
Moacir <guedesmoacir12@gmail.com>
2019-10-09 21:29:32 -0300

    fixing a little ops

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const serveStatic = require("serve-static")
 const history = require('connect-history-api-fallback')
+const serveStatic = require("serve-static")
 
 const app = express();
 
